4f4cfcc4f03d970094d1c0c01e3e45b0f25736f0
nguyen34 <nguyen_quoc_huy@hotmail.ca>
2026-05-26 22:17:52 -0700

    set up backend api call sample

--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import urls as user_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(user_urls)),
 ]
